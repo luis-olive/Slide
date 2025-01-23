d466080f8b73dd80fd7c1ed5a93dc93bbae9a105
LuisOliveira <luisoliveira.me@gmail.com>
2025-01-23 08:41:36 -0300

    at1

--- js/slide.js
+++ js/slide.js
@@ -1,35 +1,51 @@
 export default class Slide { 
   constructor(slide, wrapper){ 
-    this.slide = document.querySelector(slide)
-    this.wrapper = document.querySelector(wrapper)
+    this.slide = document.querySelector(slide);
+    this.wrapper = document.querySelector(wrapper);
+    this.dist = {finalPosition: 0, startX: 0, movement: 0}
   }
+
+  moveSlide(distX){ 
+    this.dist.Moveposition = distX;
+    this.slide.style.transform = `translate3d(${distX}px, 0, 0)`; 
+  }
+
+  updatePosition(clientX){ 
+    this.dist.movement = this.dist.startX - clientX;
+    return this.dist.finalPosition - this.dist.movement; 
+  }
+
   onStart(event){ 
     event.preventDefault();
+    this.dist.startX = event.clientX;
+    this.wrapper.addEventListener('mousemove', this.onMove);
   }
 
   onMove(event){ 
-
+    const finalPosition = this.updatePosition(event.clientX);
+    this.moveSlide(finalPosition);
   }
 
   onEnd(event){ 
-    this.wrapper.addEventListener('mousemove', this.onMove)
+    this.wrapper.removeEventListener('mousemove', this.onMove);
+    this.dist.finalPosition - this.dist.Moveposition;
   }
 
   addSlideEvent(){ 
-    this.wrapper.addEventListener('mousedown', this.onStart)
-    this.wrapper.addEventListener('mouseup', this.onEnd)
+    this.wrapper.addEventListener('mousedown', this.onStart);
+    this.wrapper.addEventListener('mouseup', this.onEnd);
   }
 
   bindEvents(){
-    this.onStart = this.onStart.bind(this)
-    this.onMove = this.onMove.bind(this)
-    this.onMove = this.onEnd.bind(this)
+    this.onStart = this.onStart.bind(this);
+    this.onMove = this.onMove.bind(this);
+    this.onEnd = this.onEnd.bind(this);
   }
 
   init(){ 
-    this.addSlideEvent();
     this.bindEvents();
-    return this
+    this.addSlideEvent();
+    return this;
   }
 
 }
